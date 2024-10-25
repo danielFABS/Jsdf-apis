23c39cc87fdbbda87c4009cc2358fd48c865ae9d
danielFABS <daniferrufino12345@gmail.com>
2024-10-25 17:28:01 -0600

    Initial commit

---
+++ app/Models/Departamento.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Departamento extends Model
+{
+    //
+    protected $connection ='mysql';
+    protected $table = 'departamento';
+    protected $primaryKey = 'id_departamento';
+    public $incrementing = true;
+    public $timestamp = false;
+
+}
